d4825956b6f06d28012cb805bd0cbab59c2d0752
Masaki Sugita <choco_9031@yahoo.co.jp>
2021-03-02 23:30:01 +0900

    Initial Commit

---
+++ AWS-Sample-Todo-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AWS-Sample-Todo-App
+//
+//  Created by Masaki on R 3/03/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
