f653641fd23dd82407802d1346d818b627fc3b54
Masaki Sugita <choco_9031@yahoo.co.jp>
2021-03-04 22:42:04 +0900

    add mode&viewFile　

---
+++ AWS-Sample-Todo-App/src/Model/Memo.swift
@@ -0,0 +1,15 @@
+//
+//  Memo.swift
+//  AWS-Sample-Todo-App
+//
+//  Created by Masaki on R 3/03/04.
+//
+
+import Foundation
+
+class MemoModel {
+    let id: Int = Int()
+    let deadline: String = String()
+    let title: String = String()
+    let memoContent: String = String()
+}
